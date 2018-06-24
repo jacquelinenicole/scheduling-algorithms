b455968ede616e1aca3be01e73fb19ce4ab2d332
Jackie van der Meulen <jackienvdm@gmail.com>
2018-06-24 14:49:51 -0400

    process struct created

--- schedulingAlgorithms.go
+++ schedulingAlgorithms.go
@@ -6,6 +6,12 @@ import (
 	"strconv"
 )
 
+type Process struct {
+	name string
+	arrival int
+	burst int
+}
+
 func main() {
     if len(os.Args) != 2 {
     	fmt.Println("Invalid argument list. Correct usage: \ngo run schedulingAlgorithms.go [file name]")
@@ -13,26 +19,26 @@ func main() {
     }
 
     fileName := os.Args[1]
-    s, runTime, algorithm, quantum, processesNames, arrivals, bursts := getInfo(fileName)
+    runTime, algorithm, quantum, processes := getInfo(fileName)
 
     fmt.Println(runTime)
     fmt.Println(algorithm)
     fmt.Println(quantum)
-    fmt.Println(processesNames)
-	fmt.Println(arrivals)
-	fmt.Println(bursts)
+
+    for _,process := range processes {
+    	fmt.Printf("Name: %3s\nArrival: %3d\nBurst: %3d\n\n", process.name, process.arrival, process.burst)
+    }
 
     if (algorithm == "fcfs") {
-    	fcfs(s, runTime, processesNames, arrivals, bursts)
+    	fcfs(runTime, processes)
     } else if (algorithm == "sjf") {
-    	sjf(s, runTime, processesNames, arrivals, bursts)
+    	sjf(runTime, processes)
     } else if (algorithm == "rr") {
-    	rr(s, runTime, quantum, processesNames, arrivals, bursts)
+    	rr(runTime, quantum, processes)
     }  else {
     	fmt.Println("Invalid algorithm name give. Accepted algorithms: \nfcfs, sjf, rr")
     	os.Exit(-1)
     }
-
 	/*
 	for s.Scan() {
 		word := s.Bytes()
@@ -48,7 +54,7 @@ func check(e error) {
 }
 
 
-func getInfo(fileName string) (*bufio.Scanner, int, string, int, []string, []int, []int) {
+func getInfo(fileName string) (int, string, int, []Process) {
     file, err := os.Open(fileName)
     check(err)
 
@@ -71,19 +77,20 @@ func getInfo(fileName string) (*bufio.Scanner, int, string, int, []string, []int
 		fmt.Println(quantum)
 	}
 
-    processesNames := make([]string, numProcesses)
-    arrivals := make([]int, numProcesses)
-    bursts := make([]int, numProcesses)
+    var processes []Process
 
     for i := 0 ; i < numProcesses ; i++ {
-    	processesNames[i] = getValue(s, "name")
-    	arrivals[i], err = strconv.Atoi(getValue(s, "arrival"))
+    	name := getValue(s, "name")
+    	arrival, err := strconv.Atoi(getValue(s, "arrival"))
     	check(err)
-    	bursts[i], err = strconv.Atoi(getValue(s, "burst"))
+    	burst, err := strconv.Atoi(getValue(s, "burst"))
     	check(err)
+
+    	processes = append(processes, Process{name, arrival, burst})
+
     }
 
-	return s, runTime, algorithm, quantum, processesNames, arrivals, bursts
+	return runTime, algorithm, quantum, processes
 }
 
 func getValue(s *bufio.Scanner, word string) string {
@@ -96,14 +103,33 @@ func getValue(s *bufio.Scanner, word string) string {
 	return string(s.Bytes())
 }
 
-func fcfs(s *bufio.Scanner, runTime int, processesNames []string, arrivals []int, bursts []int) {
+func fcfs(runTime int, processes []Process) {
 	fmt.Println("fcfs")
+
+	fmt.Printf("%3d processes\n", len(processes))
+
+	for time := 0 ; time < runTime ; time++ {
+		checkArrival(time, processes)
+
+		fmt.Printf("TIME %3d : \n", time)
+	}
+
+	fmt.Printf("Finished at time %3d", runTime)
+
 }
 
-func sjf(s *bufio.Scanner, runTime int, processesNames []string, arrivals []int, bursts []int) {
+func checkArrival(time int, processes []Process) {
+	for _,process := range processes {
+		if process.arrival == time {
+			fmt.Printf("TIME %3d : %3s arrived \n", time, process.name)
+		}
+	}
+}
+
+func sjf(runTime int, processes []Process) {
 	fmt.Println("sjf")
 }
 
-func rr(s *bufio.Scanner, runTime int, quantum int, processesNames []string, arrivals []int, bursts []int) {
+func rr(runTime int, quantum int, processes []Process) {
 	fmt.Println("rr")
 }
